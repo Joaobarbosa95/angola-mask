387830d3db19bf8e4ab712b4df64beee87a14518
Joao Campos <joaoc@evoke.pt>
2022-05-11 19:55:55 +0100

    fix

--- src/utils/wearMask.js
+++ src/utils/wearMask.js
@@ -23,7 +23,7 @@ export function wearMask(
   canvasCtx.save();
 
   canvasCtx.translate(x + width / 2, y + height / 2);
-  canvasCtx.rotate(roll * 0.9);
+  canvasCtx.rotate(roll * -0.5);
   canvasCtx.rotate((-90 * Math.PI) / 180);
   canvasCtx.scale(1, -1);
   canvasCtx.drawImage(img, -width / 2, -height / 2, width, height);
